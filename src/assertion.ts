054f99cfb5af23cff86146e4a3aa6c325a0aa771
rubelrana123 <rubelranatpi707458@gmail.com>
2025-02-24 23:21:42 +0600

    assert

--- src/assertion.ts
+++ src/assertion.ts
@@ -2,7 +2,20 @@
 // const thinkSomething = "Hello";
 // console.log((thinkSomething as string).charAt())
 
-
+type Pet = {
+   name: string;
+   walk: () => void;
+ };
+ 
+ type Fish = {
+   name: string;
+   swim: () => void;
+ };
+ 
+ let myPet: Pet | Fish = { name: 'Goldie', swim: () => console.log('Swimming') };
+ 
+ (myPet as Fish).swim();
+ 
 
 const myLuckNum = (num : string | number) : number | string | undefined => {
    if (typeof num === "number") {
